fcfaf5fd0f29c46fc7d510250c501a79be400aab
Aymeric BEAUCHAMP <aymeric.0406@gmail.com>
2018-10-22 10:34:06 +0200

    re-push d'arme

---
+++ livraison/src/modele/Rifle.java
@@ -0,0 +1,23 @@
+package modele;
+
+public class Rifle implements Weapon {
+
+	private Player owner;
+	private int damage;
+	private int range;
+
+	public Rifle(Player owner, int damage, int range) {
+		this.owner = owner;
+		this.damage = damage;
+		this.range = range;
+	}
+
+	@Override
+	public void fire(Grid g, Direction d) {
+
+	}
+
+	public void explode(Grid g) {
+		throw new UnsupportedOperationException("Not supported.");
+	}
+}
