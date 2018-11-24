dbedaa35c7de0bb78beac06cc695cdebb44d39b6
Aymeric BEAUCHAMP <aymeric.0406@gmail.com>
2018-11-24 13:24:33 +0100

    nettoyage FreeTile

--- livraison/src/modele/FreeTile.java
+++ livraison/src/modele/FreeTile.java
@@ -6,42 +6,34 @@ import java.awt.image.BufferedImage;
 * Classe fille de Tile représentant une case vide.
 */
 public class FreeTile extends Tile {
-    
+
     /**
     * Constructeur de la classe.
     * @param x
     * Ordonnée de la case.
     * @param y
     * Abscisse de la case.
-    */    
-    private BufferedImage imgRepr;
-    
+    */
+
     public FreeTile(int x, int y){
         super(x,y);
-    }    
-    
-    public FreeTile(int x, int y, BufferedImage img){    
-        this(x,y);        
-        this.imgRepr = img; 
-    }
-   
-    @Override
-    public BufferedImage getImgRepr() {    
-        return imgRepr;         
     }
 
+    public FreeTile(int x, int y, BufferedImage img){
+        super(x,y,img);
+    }
 
     /**
     * Retourne la représentation de la case vide.
     * @return Un caractère représentant la case vide.
     */
 
-    @Override  
-    public String toString() {    
-        return "_";  
+    @Override
+    public String toString() {
+        return "_";
     }
 
     public String printCoords() {
-        return "(" + this.x + "," + this.y + ")";	
+        return "(" + this.x + "," + this.y + ")";
     }
 }
