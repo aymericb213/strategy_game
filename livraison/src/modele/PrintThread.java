282f531fa2cb3855392bd05dc421cf66459f2047
Aymeric BEAUCHAMP <aymeric.0406@gmail.com>
2018-11-25 14:34:43 +0100

    javadoc PrintThread

--- livraison/src/modele/PrintThread.java
+++ livraison/src/modele/PrintThread.java
@@ -1,33 +1,41 @@
 package modele;
 
 /**
-* Processus de résolution du niveau lancé dans le mode IA battle ou en cas de demande de résolution.
+* Thread used to improve the console display if a human is playing.
 */
 public class PrintThread implements Runnable {
-    private final RealGrid g;
+    private final RealGrid grid;
     private final Player p;
 
-    public PrintThread(RealGrid g, Player p) {
-        this.g = g;
-        this.p = p;
+    /** Class constructor.
+     * @param grid
+     * The game grid.
+     * @param active_player
+     * The player currently playing his turn.
+    */
+    public PrintThread(RealGrid grid, Player active_player) {
+        this.grid = grid;
+        this.active_player = active_player;
     }
 
+    /** Prints the console interface with alternating grid displays, making the active player blink.
+     */
     @Override
     public synchronized void run() {
         while(true) {
             try {
                 System.out.println("\033[H\033[2J");
-                System.out.println("Tour " + g.getTurnNumber());
-                System.out.println(p.getName() + "\n");
-                //System.out.println(g + "\n");//vue globale
-                System.out.println(p.getView().toStringForThread() + "\n");//vues joueur
+                System.out.println("Tour " + grid.getTurnNumber());
+                System.out.println(active_player.getName() + "\n");
+                //System.out.println(grid + "\n");//vue globale
+                System.out.println(active_player.getView().toStringForThread() + "\n");//vues joueur
                 System.out.println("# : mur");
                 System.out.println("; : mine");
                 System.out.println("3 : bombe (délai avant détonation)");
                 System.out.println(". : bonus");
                 System.out.println("@ : joueur (€ si bouclier actif)");
-                System.out.println("\n" + p.printStats());
-                System.out.println(p.printControls());
+                System.out.println("\n" + active_player.printStats());
+                System.out.println(active_player.printControls());
                 Thread.sleep(800);
             } catch (InterruptedException e) {
                 break;
